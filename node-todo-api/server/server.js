b14a0b0fd4bb57e827ff6d26dbc4afd8d4588d9a
Daniel Carriola <daniel.carriola@gmail.com>
2018-03-12 12:54:26 -0600

    added route /todos/:id and tested it

--- node-todo-api/server/server.js
+++ node-todo-api/server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { ObjectID } = require('mongodb');
 
 const { mongoose } = require('./db/mongoose');
 const { Todo } = require('./models/todo');
@@ -25,6 +26,23 @@ app.get('/todos', (req, res) => {
     res.send({ todos });
   }, (err) => {
     res.status(400).send(err);
+  });
+});
+
+app.get('/todos/:id', (req, res) => {
+  let id = req.params.id;
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findById(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+
+    res.send({ todo });
+  }, (err) => {
+    res.status(400).send();
   });
 });
 
