2de5e91624d7d90333d3977aef17eb3b30923a92
Daniel Carriola <daniel.carriola@gmail.com>
2018-03-15 22:34:22 -0600

    Updated config to use json file

--- node-todo-api/server/config/config.js
+++ node-todo-api/server/config/config.js
@@ -1,9 +1,10 @@
 let env = process.env.NODE_ENV || 'development';
 
-if (env === 'development') {
-  process.env.PORT = 3000;
-  process.env.MONGODB_URI = 'mongodb://localhost:27017/todoapp';
-} else if (env === 'test') {
-  process.env.PORT = 3000;
-  process.env.MONGODB_URI = 'mongodb://localhost:27017/todoapptest';
+if (env === 'development' || env === 'test') {
+  const config = require('./config.json');
+  let envConfig = config[env];
+
+  Object.keys(envConfig).forEach((key) => {
+    process.env[key] = envConfig[key];
+  });
 }
--- node-todo-api/server/models/user.js
+++ node-todo-api/server/models/user.js
@@ -43,7 +43,7 @@ UserSchema.methods.toJSON = function() {
 UserSchema.methods.generateAuthToken = function() {
   let user = this;
   let access = 'auth';
-  let token = jwt.sign({ _id: user._id.toHexString(), access }, 'abc123').toString();
+  let token = jwt.sign({ _id: user._id.toHexString(), access }, process.env.JWT_SECRET).toString();
 
   user.tokens.push({ access, token });
   return user.save().then(() => token);
@@ -62,7 +62,7 @@ UserSchema.statics.findByToken = function(token) {
   let User = this;
 
   try {
-    let decoded = jwt.verify(token, 'abc123');
+    let decoded = jwt.verify(token, process.env.JWT_SECRET);
 
     return User.findOne({
       _id: decoded._id,
