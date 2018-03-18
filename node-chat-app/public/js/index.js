54b7350141acfcd2607dc25e60058d4ce6045473
Daniel Carriola <daniel.carriola@gmail.com>
2018-03-17 20:59:38 -0600

    Add form for messages and show incoming messages in brower

--- node-chat-app/public/js/index.js
+++ node-chat-app/public/js/index.js
@@ -7,8 +7,24 @@ socket.on('connect', function() {
 
 socket.on('newMessage', function(message) {
   console.log('New message', message);
+
+  var li = jQuery('<li></li>');
+  li.text(`${message.from}: ${message.text}`);
+
+  jQuery('#messages').append(li);
 });
 
 socket.on('disconnect', function() {
   console.log('Disconnected from server');
+});
+
+jQuery('#message-form').on('submit', function(event) {
+  event.preventDefault();
+
+  socket.emit('createMessage', {
+    from: 'User',
+    text: jQuery('[name=message]').val()
+  }, function() {
+
+  });
 });
--- node-chat-app/server/server.js
+++ node-chat-app/server/server.js
@@ -21,14 +21,10 @@ io.on('connection', (socket) => {
   socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
   socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
 
-  socket.on('createMessage', (message) => {
+  socket.on('createMessage', (message, callback) => {
     // All users
     io.emit('newMessage', generateMessage(message.from, message.text));
-    // socket.broadcast.emit('newMessage', {
-    //   from: message.from,
-    //   text: message.text,
-    //   createdAt: new Date().getTime()
-    // });
+    callback('This is from the server');
   });
 
   socket.on('disconnect', () => {
